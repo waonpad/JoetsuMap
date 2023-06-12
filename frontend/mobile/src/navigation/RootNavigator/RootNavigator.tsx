b6a2b435782311ea144317967f25b0bed1388018
waonpad <waonpad@gmail.com>
2023-06-13 03:10:58 +0900

    スクリーン名の変更と追加

--- frontend/mobile/src/navigation/RootNavigator/RootNavigator.tsx
+++ frontend/mobile/src/navigation/RootNavigator/RootNavigator.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { useAuth } from '@/lib/auth';
+import { AuthTestScreen } from '@/screens/AuthTestScreen';
 import { HomeScreen } from '@/screens/HomeScreen';
 import { PermissionDeniedScreen } from '@/screens/PermissionDeniedScreen';
-import { TestScreen } from '@/screens/TestScreen';
+import { SecureStoreTestScreen } from '@/screens/SecureStoreTestScreen';
 
 export type RootStackParamList = {
   Home: undefined; // 受け取るパラメータの型を指定できる 例: { id: number }
-  Test: undefined;
-  Test2: undefined; // PermissionDeniedTest
+  AuthTest: undefined;
+  SecureStoreTest: undefined;
+  Dummy: undefined; // PermissionDeniedTest
   PermissionDenied: undefined;
 };
 
@@ -22,8 +24,9 @@ export const RootNavigator = () => {
   return (
     <RootStack.Navigator>
       <RootStack.Screen name="Home" component={HomeScreen} />
-      <RootStack.Screen name="Test" component={TestScreen} />
-      <RootStack.Screen name="Test2" component={user ? TestScreen : PermissionDeniedScreen} />
+      <RootStack.Screen name="AuthTest" component={AuthTestScreen} />
+      <RootStack.Screen name="SecureStoreTest" component={SecureStoreTestScreen} />
+      <RootStack.Screen name="Dummy" component={user ? AuthTestScreen : PermissionDeniedScreen} />
     </RootStack.Navigator>
   );
 };
