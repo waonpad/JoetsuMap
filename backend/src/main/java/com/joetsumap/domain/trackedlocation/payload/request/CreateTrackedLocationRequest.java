c801ff2f3e89f182e19f3d60f88d8f3f10d2ba56
waonpad <waonpad@gmail.com>
2023-06-22 05:46:37 +0900

    リクエストクラスの作成

--- backend/src/main/java/com/joetsumap/domain/trackedlocation/payload/request/CreateTrackedLocationRequest.java
+++ backend/src/main/java/com/joetsumap/domain/trackedlocation/payload/request/CreateTrackedLocationRequest.java
@@ -1,8 +1,13 @@
 package com.joetsumap.domain.trackedlocation.payload.request;
 
+import com.joetsumap.common.type.LatLng;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateTrackedLocationRequest {
   
+  @NotNull
+  LatLng coords;
 }
