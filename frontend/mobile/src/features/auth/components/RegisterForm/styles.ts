44a2c290eb34b3720e69c5e3b48769b9727dc877
waonpad <waonpad@gmail.com>
2023-06-26 20:56:24 +0900

    ScrollViewの配置をFormからScreenに修正

--- frontend/mobile/src/features/auth/components/RegisterForm/styles.ts
+++ frontend/mobile/src/features/auth/components/RegisterForm/styles.ts
@@ -3,8 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // ScrollViewにしたとき怒られたので、一旦コメントアウト
-    // justifyContent: 'center',
-    // alignItems: 'justify',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
