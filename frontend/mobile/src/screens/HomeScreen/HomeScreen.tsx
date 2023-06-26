eb189be8bd5246282c65d4488f9804d9c4ac7415
waonpad <waonpad@gmail.com>
2023-06-26 20:58:42 +0900

    マップサンプルを追加

--- frontend/mobile/src/screens/HomeScreen/HomeScreen.tsx
+++ frontend/mobile/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react';
 
 import { Image } from 'expo-image';
-import { View, Button, Text } from 'react-native';
+import { View } from 'native-base';
+import { ScrollView, Button, Text } from 'react-native';
 
-// import { Map } from '@/components/Map';
+import { Map } from '@/components/Map';
 import { APP_NAME, IMAGE_SOURCE_BINARY } from '@/constants';
 // import { ModelCourseList } from '@/features/model_course/components/ModelCourseList';
+import type { TravelSpot } from '@/features/travel_spot';
+import { TravelSpotCard } from '@/features/travel_spot/components/TravelSpotCard';
+import { TravelSpotMapMarker } from '@/features/travel_spot/components/TravelSpotMapMarker';
 import { useAuth } from '@/lib/auth';
 import { secureStore } from '@/lib/expo-secure-store';
 import { useNotification } from '@/lib/notification';
@@ -14,6 +18,9 @@ import { API_URL } from '@/utils/compute';
 
 import { styles } from './styles';
 
+/**
+ * テスト用にいろいろ配置している。実際にはホーム画面
+ */
 export const HomeScreen = () => {
   const auth = useAuth();
 
@@ -22,6 +29,8 @@ export const HomeScreen = () => {
   const notification = useNotification();
 
   const [location, setLocation] = useState<any>();
+
+  const [displayTravelSpot, setDisplayTravelSpot] = useState<TravelSpot>();
 
   useEffect(() => {
     const getLocation = async () => {
@@ -33,15 +42,30 @@ export const HomeScreen = () => {
     getLocation();
   }, []);
 
+  const dummyTravelSpot: TravelSpot = {
+    id: 1,
+    name: 'test',
+    address: 'test',
+    tel: 'test',
+    type: 'FUN',
+    photo: 'test',
+    coords: {
+      lat: 37.14804525484053,
+      lng: 138.23628563899265,
+    },
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text>{JSON.stringify(auth.user)}</Text>
       {/* https://docs.expo.dev/versions/latest/sdk/image/ */}
       <Image
         style={{ width: 100, height: 100 }}
         source={{
           uri: `${API_URL}${IMAGE_SOURCE_BINARY}?imagePath=${auth.user?.icon}`,
-          headers: { Authorization: `Bearer ${auth.token}` },
+          // headers: { Authorization: `Bearer ${auth.token}` }, // 画像リソースへのアクセスは認可不要に
         }}
       />
       <Text>{APP_NAME}</Text>
@@ -70,8 +94,38 @@ export const HomeScreen = () => {
           notification.sendPushNotification();
         }}
       />
-      {/* <Map />
-      <ModelCourseList /> */}
-    </View>
+      {/* <Map /> */}
+      <View style={{ flex: 1, position: 'relative' }}>
+        <Map>
+          <TravelSpotMapMarker
+            travelSpot={dummyTravelSpot}
+            coordinate={{
+              latitude: dummyTravelSpot.coords.lat,
+              longitude: dummyTravelSpot.coords.lng,
+            }}
+            onPress={() => {
+              console.log('onPress');
+              setDisplayTravelSpot(dummyTravelSpot);
+            }}
+          />
+        </Map>
+        {displayTravelSpot && (
+          // absoluteで中心に表示
+          <View
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: [{ translateX: -50 }, { translateY: -50 }],
+            }}>
+            <TravelSpotCard
+              travelSpot={displayTravelSpot}
+              handleClose={() => setDisplayTravelSpot(undefined)}
+            />
+          </View>
+        )}
+      </View>
+      {/* <ModelCourseList /> */}
+    </ScrollView>
   );
 };
