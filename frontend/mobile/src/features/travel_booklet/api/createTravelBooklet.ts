14e2a8e488b704812c5e5bc92890abc49ab518d5
waonpad <waonpad@gmail.com>
2023-07-07 02:29:09 +0900

    一時的に書いていたコードを削除

--- frontend/mobile/src/features/travel_booklet/api/createTravelBooklet.ts
+++ frontend/mobile/src/features/travel_booklet/api/createTravelBooklet.ts
@@ -1,10 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
-import { Alert } from 'react-native';
 
 import { axios } from '@/lib/axios';
 import { queryClient, type MutationConfig } from '@/lib/react-query';
-import { EXPECTED_EXCEPTION } from '@/types';
-import { enableUseErrorBoundary } from '@/utils/compute';
 
 import { API_ENDPOINT, QUERY_KEY_PLURAL } from '../constants';
 
@@ -28,31 +25,11 @@ type UseCreateTravelBookletOptions = {
   config?: MutationConfig<typeof createTravelBooklet>;
 };
 
-// // 予期される例外(ErrorBoundaryを発火させない例外)を列挙する
-// // これらの例外に対しては個別に処理を行う
-// const expectedExceptionsOfcreateTravelBooklet: (keyof typeof EXPECTED_EXCEPTION)[] = [
-//   EXPECTED_EXCEPTION.TEST,
-//   EXPECTED_EXCEPTION.VALIDATION_ERROR,
-//   // EXPECTED_EXCEPTION.TARGET_NOT_FOUND, updateなら必要
-//   // EXPECTED_EXCEPTION.ACCESS_DENIED, updateなら必要
-//   EXPECTED_EXCEPTION.FILE_UPLOAD_ERROR, // 画像をアップロードするので必要
-// ];
-
 export const useCreateTravelBooklet = ({ config }: UseCreateTravelBookletOptions = {}) => {
   return useMutation({
     onSuccess: () => {
       queryClient.invalidateQueries([QUERY_KEY_PLURAL]);
     },
-    // useErrorBoundary: (error) => {
-    //   return enableUseErrorBoundary(error, expectedExceptionsOfcreateTravelBooklet);
-    // },
-    // onError: (error) => {
-    //   const type = error.response?.data.error.type;
-
-    //   if (type === EXPECTED_EXCEPTION.TEST) {
-    //     Alert.alert('テストエラーです。');
-    //   }
-    // },
     ...config,
     mutationFn: createTravelBooklet,
   });
